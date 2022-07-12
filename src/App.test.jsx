c2818060ce682d1f282924438720ec425754add2
Rogerio Pinheiro <rogerio.pinheirodangui@gmail.com>
2022-07-12 17:38:59 -0300

    Primeiro commit

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,8 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
 
- test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("<App />", () => {
+  it("Renders <App /> component correctly", () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/Getting started with React testing library/i)).toBeInTheDocument();
+  });
 });
